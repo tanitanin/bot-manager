4b065576c707e4c8b055e08a22d13b3de1b17bd4
tanitanin <tajima2210@gmail.com>
2013-07-05 20:20:27 +0900

    Initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BotManager::Application.config.secret_key_base = '4cee550ed2da985d3c962749c885be72fd2b24cdf09c75d3c4edac90d4a5e549baf93c4908f3ec06b05caa1816d0247a313e681af4c768e594b562a5cf650625'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BotManager::Application.config.session_store :cookie_store, key: '_bot-manager_session'
