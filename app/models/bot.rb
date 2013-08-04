01a7aa5837a5f5d60b12e4109c0909e0c5bd75e6
tanitanin <tajima2210@gmail.com>
2013-08-04 16:28:22 +0900

    modify stream and daemon kill

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -15,21 +15,18 @@ class Bot < ActiveRecord::Base
     return bot
   end
 
-  def get_default_consumer_and_token
-    cons = BotConsumer.where(bot_id: id, is_default: true).first
-    tok = BotAuthToken.where(bot_id: id, bot_consumer_id: cons.id).first
-    {consumer: cons, token: tok}
-  end
-
   def on_timeline_status(status)
-    BotStream.add(:timeline,status)
+    logger.info("userstream on_timeline_status:#{status.id}")
+    #BotStream.add(:timeline,status)
   end
 
   def on_direct_message(direct_message)
+    logger.info("userstream on_direct_message")
     @client.update("D受け取った")
   end
 
   def on_favorite(event)
+    logger.info("userstream on_favorite")
     source_user = event[:source]
     target_user = event[:target]
     status = event[:target_object]
@@ -41,6 +38,7 @@ class Bot < ActiveRecord::Base
   end
 
   def on_unfavorite(event)
+    logger.info("userstream on_unfavorite")
     source_user = event[:source]
     target_user = event[:target]
     status = event[:target_object]
@@ -52,63 +50,81 @@ class Bot < ActiveRecord::Base
   end
 
   def on_follow(event)
+    logger.info("userstream on_follow")
     source_user = event[:source]
     target_user = event[:target]
   end
 
   def on_unfollow(event)
+    logger.info("userstream on_unfollow")
     #source_user = event[:source]
     target_user = event[:target]
   end
 
   def on_block(event)
+    logger.info("userstream on_block")
     #source_user = event[:source]
     target_user = event[:target]
   end
 
   def on_unblock(event)
+    logger.info("userstream on_unblock")
     #source_user = event[:source]
     target_user = event[:target]
   end
 
   def on_delete(status_id, user_id)
+    logger.info("userstream on_delete")
     @client.update("ツイ消しを見たよ")
   end
+
   def on_user_update(event)
+    logger.info("userstream on_user_update")
     @me = @client.verify_credentials({:skip_status=>true})
   end
 
   def on_limit(skip_count)
+    logger.info("userstream on_limit:#{skip_count}")
     @client.update("規制くらった")
   end
 
   def on_reconnect(timeout,retries)
+    logger.info("userstream reconnect #{timeout} #{retries}")
   end
 
   def on_error(message)
+    logger.error("userstream error:#{message}")
   end
 
   def userstream
-    tmp = get_default_consumer_and_token
-    consumer = tmp.consumer
-    token = tmp.token
+    logger.info("Bot.userstream start")
+
+    # get consumer and token
+    consumer = BotConsumer.where(user_id: user_id, is_default: true).first
+    logger.info("bot:#{id} get_default_consumer:#{consumer.id}")
+    token = BotAuthToken.where(bot_id: id, bot_consumer_id: consumer.id).first
+    logger.info("bot:#{id} get_default_token:#{token.id}")
 
     # 自分のデータを取得
+    logger.info("Bot.userstream get bot data")
     @client = Twitter::Client.new
     @client.consumer_key = consumer.key
     @client.consumer_secret = consumer.secret
     @client.oauth_token = token.token
     @client.oauth_token_secret = token.secret
+    logger.info("@client: consumer and token set")
+
     @me = @client.verify_credentials({:skip_status=>true})
+    logger.info("Bot.userstream uid:#{@me.id} verify ok")
 
     # 以下marcov botからパクったまま
-    # TODO: なおす
     @stream = TweetStream::Client.new
     @stream.auth_method = :oauth
     @stream.consumer_key = consumer.key
     @stream.consumer_secret = consumer.secret
     @stream.oauth_token = token.token
     @stream.oauth_token_secret = token.secret
+    logger.info("Bot.userstream uid:#{@me.id} stream consumer and token set")
 
     # それぞれの処理を登録
     @stream.on_timeline_status{|status| on_timeline_status(status)}
@@ -123,9 +139,12 @@ class Bot < ActiveRecord::Base
     @stream.on_limit{|skip_count| on_limit(skip_count)}
     @stream.on_reconnect{|timeout,retries| on_reconnect(timeout,retries)}
     @stream.on_error{|message| on_error(message)}
+    logger.info("Bot.userstream uid:#{@me.id} stream setting complete")
 
     # stream取得を開始
+    logger.info("Bot.userstream uid:#{@me.id} userstream run")
     @stream.userstream
+    logger.info("Bot.userstream uid:#{@me.id} userstream ended")
   end
 
 end
--- app/models/daemon.rb
+++ app/models/daemon.rb
@@ -2,24 +2,31 @@ class Daemon < ActiveRecord::Base
   belongs_to :bot
 
   def self.start(bot_id,command,*args)
+    logger.info("Daemon.start #{bot_id} #{command}")
 
     if ! Daemon.where(:command => command).empty? then
+      logger.info("Daemon.start #{command} already exists") 
       return nil
     end
 
+    logger.info("Daemon.start fork") 
     tmp = fork do
       daemon = Daemon.new
       daemon.bot_id = bot_id
       daemon.command = command
       daemon.pid = Process.pid
       daemon.save
+      logger.info("pid:#{daemon.pid} saved")
 
-      if command == :userstream then
+      if command == "userstream" then
+        logger.info("pid:#{Process.pid} userstream")
         bot = Bot.find(bot_id)
         bot.userstream
+        logger.info("pid:#{Process.pid} userstream ended")
       end
 
       Daemon.delete_all(pid: Process.pid)
+      logger.info("pid:#{Process.pid} delete_all")
     end
     Process.detach(tmp)
   end
@@ -28,16 +35,20 @@ class Daemon < ActiveRecord::Base
     Daemon.where(
       :bot_id => bot_id,
       :command => command
-    ).find_each do |daemon|
+    ).each do |daemon|
       daemon.kill
     end
   end
 
   def kill
-  rescue
-    Process.kill('KILL',pid)
-  ensure
-    delete
+    begin
+      Process.kill('KILL',pid)
+    rescue
+      logger.info("pid:#{pid} kill error")
+    ensure
+      delete
+      logger.info("pid:#{pid} killed")
+    end
   end
 
 end
